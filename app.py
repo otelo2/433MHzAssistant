ae4096849ddee5ab58b4fb201505b4d7b3da34c4
Otelo2 <josean2009@live.com.mx>
2023-01-22 16:43:50 -0600

    refactor & add ntfy

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask, request
 from sys import platform
 import config
 import subprocess
+import datetime
+import requests
 
 # create the Flask app
 app = Flask(__name__)
@@ -12,112 +14,153 @@ def index():
     return 'Under construction'
 
 @app.route('/alarm/disarm')
-def query_example1():
-    # Expect: http://localhost:5000/alarm/disarm?code=SECRETCODEFROMCONFIG
-    # Get the code from the URL
-    verification_code = request.args.get('code')
-    # Check if the code is correct
-    if verification_code == config.expected_verification_code:
-        # Check if the OS where this script is running is Linux
-        output = ""
-        if platform == "linux":
-            # Run the disarm script
-            command = f"{config.commands_directory}/disableAlarm.sh"
-            output = subprocess.getoutput(command)
+def disarmAlarm():
+    output = ""
+    action = "disarmed"
+    commandName = "disableAlarm.sh"
+    ip_addr = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
+    
+    if (code_is_valid(request.args.get('code')) and running_on_linux()):
+        output = execute_command(commandName);
+        if command_ran_successfully():
+            html_output = f"<h1> Alarm (maybe) {action}. </h1> <h2>Confirm with your ears.</h2>"
+            send_notification(action, request.args.get('sentBy'), ip_addr)
         else:
-            return "Not supported on this platform"
-        
-        # Return the output of the disarm script
-        if "End of file" in output:
-            html_output = f"<h1> Alarm (maybe) disarmed. </h1> <h2>Confirm with your ears.</h2>"
-        else:
-            html_output = f"Alarm (maybe) not disarmed. An error probably happened. Check logs."
-            
-        html_output += f"Here is the output from the command: \n {output}"	
+            html_output = f"Alarm (maybe) not {action}. An error probably happened. Check logs."
+        html_output += f"\nHere is the output from the command: \n {output}"
         return html_output
     else:
-        return 'Why are we here? Just to suffer?' 
+        snooper_alert_notification(ip_addr)
+        return '<h1>Why are we here? Just to suffer?</h1>'
     
 @app.route('/alarm/arm/full')
-def query_example2():
-    # Expect: http://localhost:5000/alarm/disarm?code=SECRETCODEFROMCONFIG
-    # Get the code from the URL
-    verification_code = request.args.get('code')
-    # Check if the code is correct
-    if verification_code == config.expected_verification_code:
-        # Check if the OS where this script is running is Linux
-        output = ""
-        if platform == "linux":
-            # Run the disarm script
-            command = f"{config.commands_directory}/enableFullAlarm.sh"
-            output = subprocess.getoutput(command)
+def fullAlarmArm():
+    output = ""
+    action = "armed full"
+    commandName = "enableFullAlarm.sh"
+    ip_addr = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
+    
+    if (code_is_valid(request.args.get('code')) and running_on_linux()):
+        output = execute_command(commandName);
+        if command_ran_successfully():
+            html_output = f"<h1> Alarm (maybe) {action}. </h1> <h2>Confirm with your ears.</h2>"
+            send_notification(action, request.args.get('sentBy'), ip_addr)
         else:
-            return "Not supported on this platform"
-        
-        # Return the output of the disarm script
-        if "End of file" in output:
-            html_output = f"<h1> Full Alarm (maybe) armed. </h1> <h2>Confirm with your ears.</h2>"
-        else:
-            html_output = f"Full Alarm (maybe) not armed. An error probably happened. Check logs."
-            
-        html_output += f"Here is the output from the command: \n {output}"	
+            html_output = f"Alarm (maybe) not {action}. An error probably happened. Check logs."
+        html_output += f"\nHere is the output from the command: \n {output}"
         return html_output
     else:
-        return 'Why are we here? Just to suffer?' 
-    
+        snooper_alert_notification(ip_addr)
+        return '<h1>Why are we here? Just to suffer?</h1>'
+
+
 @app.route('/alarm/arm/downstairs')
-def query_example3():
-    # Expect: http://localhost:5000/alarm/disarm?code=SECRETCODEFROMCONFIG
-    # Get the code from the URL
-    verification_code = request.args.get('code')
-    # Check if the code is correct
-    if verification_code == config.expected_verification_code:
-        # Check if the OS where this script is running is Linux
-        output = ""
-        if platform == "linux":
-            # Run the disarm script
-            command = f"{config.commands_directory}/enableDownstairsAlarm.sh"
-            output = subprocess.getoutput(command)
-        else:
-            return "Not supported on this platform"
-        
-        # Return the output of the disarm script
-        if "End of file" in output:
-            html_output = f"<h1> Downstairs Alarm (maybe) armed. </h1> <h2>Confirm with your ears.</h2>"
-        else:
-            html_output = f"Downstairs Alarm (maybe) not armed. An error probably happened. Check logs."
-            
-        html_output += f"Here is the output from the command: \n {output}"	
-        return html_output
-    else:
-        return 'Why are we here? Just to suffer?' 
+def downstairsAlarmArm():
+    output = ""
+    action = "armed downstairs"
+    commandName = "enableDownstairsAlarm.sh"
+    ip_addr = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
     
-@app.route('/alarm/emergency')
-def query_example4():
-    # Expect: http://localhost:5000/alarm/disarm?code=SECRETCODEFROMCONFIG
-    # Get the code from the URL
-    verification_code = request.args.get('code')
-    # Check if the code is correct
-    if verification_code == config.expected_verification_code:
-        # Check if the OS where this script is running is Linux
-        output = ""
-        if platform == "linux":
-            # Run the disarm script
-            command = f"{config.commands_directory}/enableEmergencyAlarm.sh"
-            output = subprocess.getoutput(command)
+    if (code_is_valid(request.args.get('code')) and running_on_linux()):
+        output = execute_command(commandName);
+        if command_ran_successfully():
+            html_output = f"<h1> Alarm (maybe) {action}. </h1> <h2>Confirm with your ears.</h2>"
+            send_notification(action, request.args.get('sentBy'), ip_addr)
         else:
-            return "Not supported on this platform"
-        
-        # Return the output of the disarm script
-        if "End of file" in output:
-            html_output = f"<h1> Downstairs Alarm (maybe) armed. </h1> <h2>Confirm with your ears.</h2>"
-        else:
-            html_output = f"Downstairs Alarm (maybe) not armed. An error probably happened. Check logs."
-            
-        html_output += f"Here is the output from the command: \n {output}"	
+            html_output = f"Alarm (maybe) not {action}. An error probably happened. Check logs."
+        html_output += f"\nHere is the output from the command: \n {output}"
         return html_output
     else:
-        return 'Why are we here? Just to suffer?' 
+        snooper_alert_notification(ip_addr)
+        return '<h1>Why are we here? Just to suffer?</h1>'
+
+@app.route('/alarm/emergency')
+def emergencyAlarm():
+    output = ""
+    action = "emergency activated"
+    commandName = "enableEmergencyAlarm.sh"
+    ip_addr = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
+    
+    if (code_is_valid(request.args.get('code')) and running_on_linux()):
+        output = execute_command(commandName);
+        if command_ran_successfully():
+            html_output = f"<h1> Alarm (maybe) {action}. </h1> <h2>Confirm with your ears.</h2>"
+            send_notification(action, request.args.get('sentBy'), ip_addr)
+        else:
+            html_output = f"Alarm (maybe) not {action}. An error probably happened. Check logs."
+        html_output += f"\nHere is the output from the command: \n {output}"
+        return html_output
+    else:
+        snooper_alert_notification(ip_addr)
+        return '<h1>Why are we here? Just to suffer?</h1>'
+
+# Helper methods start
+
+def execute_command(command):
+    # Execute a command and return the output
+    command = f"{config.commands_directory}/{command}"
+    output = subprocess.getoutput(command)
+    return output
+
+def code_is_valid(verification_code):
+    # Check if the code is correct using a switch statement
+    if verification_code == config.expected_verification_code:
+        return True
+    else:
+        return False
+    
+def running_on_linux():
+    # Check if the OS where this script is running is Linux
+    if platform == "linux":
+        return True
+    else:
+        return False
+    
+def command_ran_successfully(output):
+    # Check if the command ran successfully
+    if "End of file" in output:
+        return True
+    else:
+        return False
+    
+
+# This method sends a notification to ntfy.sh using requests with the time and the name of the person who sent the message
+def send_notification(action, senderName, ip):
+    tags = ""
+    priority = "default"
+    
+    # Get the current time and format it
+    currentTime = datetime.now().strftime("%H:%M:%S")
+    
+    if action == "disarmed":
+        tags = "green_square,speaker"
+        priority = "min"
+    elif action == "armed downstairs":
+        tags = "yellow_square,loud_sound"
+    elif action == "armed full":
+        tags = "orange_square,loud_sound"
+    elif action == "emergency activated":
+        tags = "red_square,loudspeaker"
+        priority = "urgent"
+
+    requests.post(f"{config.ntfy_url}/alarm_notifications", \
+                    data=f"Alarm {action} by {senderName} at {currentTime} from IP: {ip}", \
+                    headers={
+                        "Title": f"Alarm has been {action}",
+                        "Priority": priority,
+                        "Tags": tags
+                    })
+    
+def snooper_alert_notification(ip):
+    requests.post(f"{config.ntfy_url}/alarm_notifications", \
+                    data=f"Snooper alert! From {ip}", \
+                    headers={
+                        "Title": f"Snooper alert!",
+                        "Priority": "low",
+                        "Tags": "red_square,loudspeaker"
+                    })
+
+# Helper methods end
 
 if __name__ == '__main__':
     # run app in debug mode on port 5000
